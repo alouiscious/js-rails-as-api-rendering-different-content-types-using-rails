5c10263a78ce5c8a6816caef72f91cf8a6763f35
Alouiscious Media <info@reneesoul.com>
2020-03-09 05:00:23 -0700

    completed lab workd

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,7 +1,16 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
-    render plain: "Hello #{@birds[3].name}"
+    birds = Bird.all # - instance variables are not used in json
+    # @birds = Bird.all
+    # render 'birds/index.html.erb'
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+    # json: { message: 'Hashes of data will get converted to JSON' }
+    # json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+    # plain: "Hello #{@birds[3].name}"
+    #  json: ['As','well','as','arrays']
+    #  json: @birds - instance variables are not used in json
+    #  json: birds
+    # json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    # json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json --we dont need to_json
   end
 end
